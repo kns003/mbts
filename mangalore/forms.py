04ab5e1deba65cbeb7552a839ee3def32275958c
Shashank <shashank.hegde@tarams.com>
2014-08-05 12:20:47 +0530

    adding form validation

--- mangalore/forms.py
+++ mangalore/forms.py
@@ -10,3 +10,14 @@ class RouteForm(forms.Form):
     source = forms.CharField(max_length=30)
     destination = forms.CharField(max_length = 30)
     
+    def clean_routes(self):
+        source = self.cleaned_data.get('source')
+        destination = self.cleaned_data.get('destination')
+        buses = Bus.objects.all()
+        for bus in buses:
+            if not [source,destination] in bus.routes:
+                raise forms.ValidationError("No Bus no. exists with this Source and destination.")
+            elif source == destination or destination == source:
+                raise forms.ValidationError("Source and Destination cannot be same")
+            return source
+                
